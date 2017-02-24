7004d7390d4a604bf90e66df7dd7d49bcdab50ed
admin <admin@admins-imac.elcam.co.il>
2017-02-24 12:01:49 +0200

    with animation and bronze challenge

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -1,60 +1,96 @@
 //
-//  ViewController.swift
-//  Quiz
-//
-//  Created by admin on 27 Shevat 5777.
-//  Copyright © 5777 Matan alpha. All rights reserved.
+//  Copyright © 2015 Big Nerd Ranch
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions = [
+        "From what is cognac made?",
+        "What is 7+7?",
+        "What is the capital of Vermont?"
+    ]
+    let answers = [
+        "Grapes",
+        "14",
+        "Montpelier"
+    ]
+    
+    var currentQuestionIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = questions[currentQuestionIndex]
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        currentQuestionLabel.text = questions[currentQuestionIndex]
+        updateOffScreenLabel()
     }
-
-    let questions: [String] = [
-        "What is 7+7",
-        "What is the capital of Vermont?",
-        "What is cognac made from?"
-    ]
     
-    let answers: [String] = [
-        "14",
-        "Montpelier",
-        "Grapes"
-    ]
-    var currentQuestionIndex: Int = 0
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Set the label's initial alpha
+        nextQuestionLabel.alpha = 0
+    }
     
-    @IBOutlet weak var questionLabel: UILabel!
+    func animateLabelTransitions() {
+        
+        view.layoutIfNeeded()
+        
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+        
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       usingSpringWithDamping : 0.1,
+                       initialSpringVelocity : 0.1,
+                       
+                       options: [.curveLinear],
+                       animations: {
+                        self.currentQuestionLabel.alpha = 0
+                        self.nextQuestionLabel.alpha = 1
+                        
+                        self.view.layoutIfNeeded()
+            },
+                       completion: { _ in
+                        swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+                        swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+                        
+                        self.updateOffScreenLabel()
+        })
+    }
     
-    @IBOutlet weak var answerLabel: UILabel!
     
+    func updateOffScreenLabel() {
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
+    }
     
-    @IBAction func showNextQuestion(_ sender: UIButton) {
+    @IBAction func showNextQuestion(_ sender: AnyObject) {
         currentQuestionIndex += 1
         if currentQuestionIndex == questions.count {
             currentQuestionIndex = 0
         }
-        let question: String = questions[currentQuestionIndex]
-        questionLabel.text = question
+        
+        let question = questions[currentQuestionIndex]
+        nextQuestionLabel.text = question
         answerLabel.text = "???"
+        
+        animateLabelTransitions()
     }
     
-    
-    
-    @IBAction func showAnswer(_ sender: UIButton) {
-        let answer: String = answers[currentQuestionIndex]
+    @IBAction func showAnswer(_ sender: AnyObject) {
+        let answer = answers[currentQuestionIndex]
         answerLabel.text = answer
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
